61ead4874aa5791132af01688b30303bd9a7efb9
alfredo <dx3911freddy@gmail.com>
2026-02-27 23:43:28 -0800

    adding 8kyu

---
+++ Grasshopper-Array-Mean.js
@@ -0,0 +1,12 @@
+/*
+function findAverage(nums) {
+  // Code here
+  
+  return nums.reduce((x,accumulator) => x + accumulator)/nums.length
+}
+  */
+ function findAverage(nums) {
+  // Code here
+  
+  return nums.reduce((x,accumulator) => x + accumulator)/nums.length
+}
